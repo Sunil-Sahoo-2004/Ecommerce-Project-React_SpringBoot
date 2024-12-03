4cc5649f15c7d0428941e493a17602b5a3a8ce38
Sunil Sahoo <sunilsahoo1525@gmail.com>
2024-12-03 21:55:02 +0530

    first commit

---
+++ ecom-project-server/src/main/java/com/sahoo/ecom_project/service/ProductService.java
@@ -0,0 +1,48 @@
+package com.sahoo.ecom_project.service;
+
+import com.sahoo.ecom_project.model.Product;
+import com.sahoo.ecom_project.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageDate(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        productRepository.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return productRepository.searchProducts(keyword);
+    }
+}
---
+++ ecom-project-server/src/main/resources/data1.sql
@@ -0,0 +1,7 @@
+INSERT INTO product (available, brand, category, desc, name, price, quantity, release_date)
+VALUES
+    (TRUE, 'Apple', 'Electronics', 'Latest model iPhone', 'iPhone 15', 999.99, 50, '2024-11-01 10:00:00'),
+    (TRUE, 'Samsung', 'Electronics', 'Flagship model Galaxy', 'Galaxy S23', 899.99, 40, '2024-10-15 09:30:00'),
+    (FALSE, 'Sony', 'Gaming', 'PlayStation 5 console', 'PlayStation 5', 499.99, 0, '2024-09-20 15:00:00'),
+    (TRUE, 'Dell', 'Computers', 'High-performance laptop', 'XPS 13', 1199.99, 25, '2024-11-05 14:45:00'),
+    (TRUE, 'Nike', 'Fashion', 'Running shoes', 'Air Zoom Pegasus', 129.99, 100, '2024-11-07 12:00:00');
